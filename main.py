e4cb3e73062fc5bccef42a8bba6f79e68e5274fe
Sanjufied <roseredfantasy@gmail.com>
2025-07-04 21:17:18 +0530

    Added mainfile and requirements

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import OpenGL.GL as gl
+import numpy as np
+
+class main:
+    def __init__(self):
+        pygame.init()
+        pass
+    
+    def run(self):
+        pass
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    Player = main()
+    Player.run()
+    
+    pygame.quit()
